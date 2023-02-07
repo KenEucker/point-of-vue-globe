5e796cdac52d5009a1148f0091867e32fbcde1fb
Ken Eucker <keneucker@gmail.com>
2023-02-07 10:48:43 -0800

    submitting vue [HelloWorld] version 0.0.1 for publishing

---
+++ packages/lib/src/components/uncategorized/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="flex justify-center">
+      <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+        <h1>Hi, {{ msg }}</h1>
+<div>
+    <h2>Your Posts</h2>
+    <div v-for="post in query.posts">
+        {{ post.title }}
+    </div>
+</div>
+
+      </div>
+    </div>
+  </template>
+  <style scoped>
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+  </style>
+  <script setup>
+    /// Auto Import
+    import { onMounted, ref, computed } from 'vue'
+    import { useMotion } from '@vueuse/motion'
+
+    /// Hydration
+    const query = {}
+    const vue = {}
+    
+    /// Script
+    const msg = 'Kendrid Spirit Is Going To Burning Man 2023'
+
+
+    // onMounted(() => {
+      // console.log('window.tailwindCSS', window.tailwindCSS)
+      // window.tailwindCSS.refresh()
+    // })
+  </script>
